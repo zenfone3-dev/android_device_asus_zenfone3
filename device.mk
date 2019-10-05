d033e77f3ff3f7c8b9185b28c4ef31f1706687c3
Michael Bestas <mkbestas@lineageos.org>
2019-10-05 22:50:26 +0300

    zenfone3: Update to power HAL 1.2
    Change-Id: Ic8c5f7e9c77a95befff5661e1634683ec6c25ac4

--- device.mk
+++ device.mk
@@ -337,7 +337,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 # Properties
 -include $(LOCAL_PATH)/prop.mk
